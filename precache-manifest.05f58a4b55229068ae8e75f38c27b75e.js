13e6f923b07759b2126aee540f72a14c983a76ee
Hrishi Raj <hrishiraj1570@gmail.com>
2022-05-20 22:42:18 +0530

    Updates

---
+++ precache-manifest.05f58a4b55229068ae8e75f38c27b75e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c67b76810fc813910b0c",
+    "url": "/Blog-Post/static/css/main.58a9348d.chunk.css"
+  },
+  {
+    "revision": "c67b76810fc813910b0c",
+    "url": "/Blog-Post/static/js/main.c67b7681.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/Blog-Post/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "e7358852818b939b2b5f",
+    "url": "/Blog-Post/static/js/2.e7358852.chunk.js"
+  },
+  {
+    "revision": "0b27fd44c05ce8bb7424",
+    "url": "/Blog-Post/static/js/runtime~main.0b27fd44.js"
+  },
+  {
+    "revision": "760e915d01eb6ba26fe1dd111674999d",
+    "url": "/Blog-Post/index.html"
+  }
+];
